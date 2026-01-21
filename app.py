6de60dca4784a20c818c069eb41f1542b231cae3
Ange Einstein <ichfrissdich@gmail.com>
2026-01-21 18:11:10 +0100

    feat: Change SocketIO async mode from 'eventlet' to 'threading'

--- app.py
+++ app.py
@@ -1717,7 +1717,7 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # Thread lock for data updates
 thread_lock = Lock()
